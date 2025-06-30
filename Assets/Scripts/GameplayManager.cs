3c06339f52732c0b4cbfff3236afddef9fe7a7eb
Aditya Sharma <adityas.cse@gmail.com>
2025-06-30 20:18:19 +0530

    randomizing target position

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -5,11 +5,15 @@ public class GameplayManager : MonoBehaviour
     [SerializeField] GameObject targetPrefab;
     private void Start()
     {
-        SpawnTarget();
+        InvokeRepeating("SpawnTarget", 1f, 1f);
     }
 
     void SpawnTarget()
     {
-        Instantiate(targetPrefab);
+        float xPos = Random.Range(-7.5f, 7.5f);
+        float yPos = Random.Range(-4f, 4f);
+
+        Vector3 randomTargetPos = new Vector3(xPos, yPos, 0);
+        Instantiate(targetPrefab, randomTargetPos, Quaternion.identity);
     }
 }
